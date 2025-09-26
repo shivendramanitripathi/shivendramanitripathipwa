552c76224208832b7844de3f173537ffdbbcb437
Shivendra mai tripathi <52592390+shivendramanitripathi@users.noreply.github.com>
2025-09-26 13:42:52 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1103778187"
+    serviceWorkerVersion: "2642965294"
   }
 });
